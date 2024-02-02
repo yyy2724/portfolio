0152bf89a53f8be1e7c496d1c63481258406e77a
yyy2724 <yyy2724@naver.com>
2024-02-02 15:15:36 +0900

    finish

--- src/main/java/com/example/portfolio/HomeController.java
+++ src/main/java/com/example/portfolio/HomeController.java
@@ -1,12 +1,32 @@
 package com.example.portfolio;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
     @GetMapping({"", "/","/index"})
     public String index(){
 
         return "index";
+    }
+
+    @GetMapping("/project1")
+    public String project1(){
+
+        return "/project1";
+    }
+
+    @GetMapping("/project2-3")
+    public String project23(){
+
+        return "/project2-3";
+    }
+
+    @GetMapping("/movingMate")
+    public String movingMate(){
+
+        return "/movingMate";
     }
 }
