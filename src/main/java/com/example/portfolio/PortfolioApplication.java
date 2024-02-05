6d0fb0b0ca0eb8e9a6dc72e6bd3e282d4b7e37cd
yyy2724 <yyy2724@naver.com>
2024-02-05 16:21:47 +0900

    9

--- src/main/java/com/example/portfolio/PortfolioApplication.java
+++ src/main/java/com/example/portfolio/PortfolioApplication.java
@@ -2,8 +2,12 @@ package com.example.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = false)
 public class PortfolioApplication {
 
     public static void main(String[] args) {
